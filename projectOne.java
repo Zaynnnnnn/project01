dc6fcf2a2e2a061dab6398a7306c6bf1b1500363
zayn <uzayn012345@gmail.com>
2022-03-08 14:12:58 +0500

    My First Commit

---
+++ projectOne.java
@@ -0,0 +1,23 @@
+package zaynProjects;
+
+public class projectOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello EveryOne, this is Zayn... ");
+		
+       //  Project one task is create two variable and add them respectively...
+		
+       //  Two Variables...		  
+		int first = 77;
+		int second = 11;
+		
+       //  Variable for add two Variable...
+		int add = first + second;
+		
+       //  For Print...
+		
+		System.out.println("Sum of Two Variables is : " + add);
+	}
+
+}
